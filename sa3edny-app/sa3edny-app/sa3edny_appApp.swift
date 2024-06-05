1cc24257e9a338bb48229714eeb1405f000db102
Sherif Yasser <sherifyasser92@gmail.com>
2024-06-05 16:59:16 +0200

    created swiftui XCode project

---
+++ sa3edny-app/sa3edny-app/sa3edny_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  sa3edny_appApp.swift
+//  sa3edny-app
+//
+//  Created by Sherif Yasser on 05.06.24.
+//
+
+import SwiftUI
+
+@main
+struct sa3edny_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
