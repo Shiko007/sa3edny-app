29777d562439b5d597f61a36a505127fa8b78fbf
Sherif Yasser <sherifyasser92@gmail.com>
2024-06-05 21:19:20 +0200

    Added skeleton for the services

--- sa3edny-app/sa3edny-app/ContentView.swift
+++ sa3edny-app/sa3edny-app/ContentView.swift
@@ -8,17 +8,42 @@
 import SwiftUI
 
 struct ContentView: View {
+    var platforms: [Platform] = [.init(name: "Carpenter", imageName: "chair.fill", color: .brown),
+                                 .init(name: "Electrician", imageName: "bolt.slash.fill", color: .yellow),
+                                 .init(name: "Plumber", imageName: "sink.fill", color: .cyan),
+                                 .init(name: "Maintainance", imageName: "tv.fill", color: .indigo),
+                                 .init(name: "Pet Grooming", imageName: "cat.fill", color: .gray),
+                                 .init(name: "Cleaner", imageName: "washer.fill", color: .teal),
+                                 .init(name: "Gardner", imageName: "tree.fill", color: .green)]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            ForEach(platforms, id: \.name) {platform in
+                VStack{
+                    NavigationLink(value: platform) {
+                        Label(platform.name, systemImage: platform.imageName)
+                            .foregroundColor(platform.color)
+                    }
+                }.padding()
+            }
+            .navigationTitle("Services")
+                .navigationDestination(for: Platform.self){ platform in
+                    ZStack{
+                        platform.color.ignoresSafeArea()
+                        VStack{
+                            Label(platform.name, systemImage: platform.imageName).font(.largeTitle).bold()
+                        }.padding()
+                    }
+                }
         }
-        .padding()
     }
 }
-
+struct Platform: Hashable {
+    let name: String
+    let imageName: String
+    let color: Color
+}
+    
 #Preview {
     ContentView()
 }
